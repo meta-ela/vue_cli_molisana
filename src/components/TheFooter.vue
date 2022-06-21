a155dae19abcefe1f82b29f537d1f3bfb7f5640f
meta-ela <anxhelameta@live.it>
2022-06-21 10:49:25 +0200

    Create repo, img folder and header/footer.vue

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        il mio footer
+    </div>
+</template>
+
+<script>
+export default {
+    // buona norma aggiungere chiave name: "stesso nome del file"
+    name: "TheFooter",
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <!--  creare una cartella img in public  -->
+        <!-- senza slash quindi img/la_molisana-logo.png diventa un percorso relativo -->
+        <!-- usare sempre slash quindi /img/la_molisana..ecc -->
+        <img src="/img/la-molisana-logo.png" alt="logo la molisana">
+    </div>
+</template>
+
+<script>
+export default {
+    // buona norma aggiungere chiave name: "stesso nome del file"
+    name: "TheHeader",
+}
+</script>
+
+<style>
+
+</style>
